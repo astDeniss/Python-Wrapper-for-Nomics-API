befae5542c4aa0e32bf58e1e3e23b17f5b8a900a
Dennis Ast <astasitsev@gmail.com>
2020-01-26 15:35:24 +0200

    Moved configuration file/Changed class inititalization

--- API/__init__.py
+++ API/__init__.py
@@ -1,6 +1,8 @@
-from configuration import NOMICS_API_KEY
-
 class BaseServerUrl:
-    def __init__(self):
-        self.api_key = NOMICS_API_KEY
+    def __init__(self, nomics_api_key):
+        """
+
+        :param nomics_api_key: string
+        """
+        self.api_key = nomics_api_key
         self.base_url = "https://api.nomics.com/v1" #All requests should be prefixed by the server URL
--- configuration.py
+++
@@ -1,7 +0,0 @@
-"""
-This is where your API key is stored.
-Get your API KEY for Nomics crypto data at: https://nomics.com/
-When using git repository- don't forget to add this file to .gitignore
-"""
-
-NOMICS_API_KEY = "Place your API key here"
